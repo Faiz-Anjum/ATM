40b4da05a364e12c4e3adb6340330f0348c745db
Faiz-Anjum <faizanjum111@gmail.com>
2021-02-19 22:05:38 +0530

    Easy to install

--- setup.py
+++ setup.py
@@ -4,10 +4,25 @@ import sys
 from os import system
 
 
+def cls():
+    if sys.platform.startswith('win32' or 'win64' or 'win86'):
+        os.system('cls')
+    else:
+        os.system('clear')
+
 try:
     os.system('pip3 install -r requirements.txt || pip install -r requirements.txt')
 except RuntimeError:
     pass
+cls()
+
+try:
+    from pyfiglet import Figlet
+    f = Figlet(font='big')
+    print(f.renderText('Welcome'))
+except ImportError or ModuleNotFoundError:
+    pass
+
 
 
 
@@ -31,16 +46,9 @@ from asset.name2 import name2              # This is for names
 from asset.name3 import name3
 from asset.name4 import name4
 
-def cls():
-    if sys.platform.startswith('win32' or 'win64' or 'win86'):
-        os.system('cls')
-    else:
-        os.system('clear')
 
 
-cls()
-
-print('Welcome to the Merryweather ATM setup wizard.\nPlease enter the following questions carefully for any wrong information may create further problems.')
+print('Welcome to the Merryweather ATM setup wizard.\n\nPlease enter the following questions carefully for any wrong information may create further problems.')
 
 sleep(5)
 print('')
@@ -63,6 +71,7 @@ usr = 1
 names = []
 for i in range(5):
     o_name = input('Enter the name of user {}.\n> '.format(usr))
+    print('')
     t_name = o_name.title()
     names.append(t_name)
     with open('asset/name{}.py'.format(str(inx)), 'w') as nn:
@@ -83,6 +92,7 @@ inx2 = 0
 for i in range(5):
     try:
         o_pin = int(input('Enter the preferred PIN for {}.\n> '.format(names[inx2])))
+        print('')
         with open('asset/pin{}.py'.format(str(inx2)), 'w') as pn:
             pn.write('pin{} = {}'.format(inx2, o_pin))
             pn.close()
@@ -106,6 +116,7 @@ inx3 = 0
 for i in range(5):
     try:
         o_bal = int(input('Enter the Balance for {}.\n> $ '.format(names[inx3])))
+        print('')
         with open('asset/asset{}.py'.format(str(inx3)), 'w') as bl:
             bl.write('a{} = {}'.format(inx3, o_bal))
             bl.close()
@@ -122,9 +133,30 @@ for i in range(5):
 cls()
 
 print('Configuration successful')
-print('You can now run the ATM.py script and login with the credentials you entered.')
 print('')
-a = input("Press any key and hit enter to close this script.\n")
+print('You can now run the ATM.py script and login with the credentials you entered.')
+sleep(8)
+def run():
+    cls()
+    atm = input('Would you like to run the ATM script now?\n> ')
+    
+    if atm == 'y' or atm == 'Y' or atm == 'YES' or atm == 'Yes' or atm == 'yes':
+        os.system('python3 ATM.py || python ATM.py')
+        cls()
+        exit()
+        
+    elif atm == 'n' or atm == 'N' or atm == 'NO' or atm == 'No' or atm == 'no':
+        cls()
+        a = input("Press Enter to close this script.\n")
+
+    elif len(atm) <= 0 or atm != atm.isalpha() or atm != 'yes' or atm != 'YES' or atm != 'Yes' or atm != 'y' or atm != 'Y' or atm != 'no' or atm != 'NO' or atm != 'No' or atm != 'n' or atm != 'N':
+        cls()
+        print('Please enter either \'Yes\' or \'No\'.')
+        sleep(3)
+        run()
+    
+run()
+
 
 
 
