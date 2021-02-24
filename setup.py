929c7f3fdf58410b29213582638e754b450b831d
Faiz-Anjum <faizanjum111@gmail.com>
2021-02-24 23:38:03 +0530

    Easy to install

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from time import sleep
 import os
-import sys
+import sys                                                                      # 386 Lines
 from os import system
 
 
@@ -26,116 +26,343 @@ except ImportError or ModuleNotFoundError:
 
 
 
-from asset.asset0 import a0
-from asset.asset1 import a1
-from asset.asset2 import a2             # This is for balance
-from asset.asset3 import a3
-from asset.asset4 import a4
-
-from asset.pin0 import pin0
-from asset.pin1 import pin1
-from asset.pin2 import pin2              # This is for pins
-from asset.pin3 import pin3
-from asset.pin4 import pin4
 
 
+try:
+    from asset.asset0 import a0
+except ModuleNotFoundError or ImportError:      #balance
+    pass    
 
-from asset.name0 import name0
-from asset.name1 import name1
-from asset.name2 import name2              # This is for names
-from asset.name3 import name3
-from asset.name4 import name4
+
+try:
+    from asset.asset1 import a1
+except ModuleNotFoundError or ImportError:
+    pass
 
 
 
-print('Welcome to the Merryweather ATM setup wizard.\n\nPlease enter the following questions carefully for any wrong information may create further problems.')
+try:
+    from asset.asset2 import a2
+except ModuleNotFoundError or ImportError:
+    pass
 
-sleep(5)
-print('')
 
-print('Gathering information')
 
-sleep(3)
-print('')
+try:
+    from asset.asset3 import a3
+except ModuleNotFoundError or ImportError:
+    pass
 
-print('Compiling data')
 
-print('')
-sleep(3)
-print('Processing')
-sleep(3)
-cls()
 
-inx = 0
-usr = 1
-names = []
-for i in range(5):
-    o_name = input('Enter the name of user {}.\n> '.format(usr))
+try:
+    from asset.asset4 import a4
+except ModuleNotFoundError or ImportError:              #balance
+    pass
+
+
+
+try:
+    from asset.asset5 import a5
+except ModuleNotFoundError or ImportError:
+    pass
+
+
+
+try:
+    from asset.asset6 import a6
+except ModuleNotFoundError or ImportError:
+    pass
+
+
+
+try:
+    from asset.asset7 import a7
+except ModuleNotFoundError or ImportError:
+    pass
+
+
+
+try:
+    from asset.asset8 import a8
+except ModuleNotFoundError or ImportError:
+    pass
+
+
+
+try:
+    from asset.asset9 import a9
+except ModuleNotFoundError or ImportError:      #balance
+    pass
+
+
+
+
+
+
+
+try:
+    from asset.pin0 import pin0                 # pin
+except ModuleNotFoundError or ImportError:
+    pass
+try:
+    from asset.pin1 import pin1
+except ModuleNotFoundError or ImportError:
+    pass
+try:
+    from asset.pin2 import pin2
+except ModuleNotFoundError or ImportError:
+    pass              
+try:
+    from asset.pin3 import pin3
+except ModuleNotFoundError or ImportError:
+    pass
+try:
+    from asset.pin4 import pin4
+except ModuleNotFoundError or ImportError:              #pin
+    pass
+try:
+    from asset.pin5 import pin5
+except ModuleNotFoundError or ImportError:
+    pass
+try:
+    from asset.pin6 import pin6
+except ModuleNotFoundError or ImportError:
+    pass
+try:
+    from asset.pin7 import pin7
+except ModuleNotFoundError or ImportError:
+    pass              
+try:
+    from asset.pin8 import pin8
+except ModuleNotFoundError or ImportError:
+    pass
+try:
+    from asset.pin9 import pin9
+except ModuleNotFoundError or ImportError:     #pin
+    pass
+
+
+
+
+
+
+
+try:    
+    from asset.name0 import name0               #name
+except ModuleNotFoundError or ImportError:
+    pass
+
+try:
+    from asset.name1 import name1
+except ModuleNotFoundError or ImportError:
+    pass
+try:
+    from asset.name2 import name2
+except ModuleNotFoundError or ImportError:
+    pass              
+try:
+    from asset.name3 import name3
+except ModuleNotFoundError or ImportError:
+    pass
+try:
+    from asset.name4 import name4
+except ModuleNotFoundError or ImportError:                  #name
+    pass
+try:
+    from asset.name5 import name5
+except ModuleNotFoundError or ImportError:
+    pass
+try:
+    from asset.name6 import name6
+except ModuleNotFoundError or ImportError:
+    pass
+try:
+    from asset.name7 import name7
+except ModuleNotFoundError or ImportError:
+    pass              
+try:
+    from asset.name8 import name8
+except ModuleNotFoundError or ImportError:
+    pass
+try:
+    from asset.name9 import name9
+except ModuleNotFoundError or ImportError:
+    pass                                        #name
+
+
+
+
+def users():
+    cls()
+    try:
+        val = int(input('How many users do you want to add in this facility?\n\n> '))
+        val1 = str(val).startswith('0')
+        if val <= 10 and val != 0:
+            global f
+            f = int(val)
+            cls()
+            name()
+        elif val > 10:
+            cls()
+            print('You can only add a maximum of ten users.')
+            sleep(3)
+            users()
+        elif val1 == True:
+            cls()
+            print('Are you kidding me? Why the hell are you using this script then???')
+            sleep(5)
+            print('')
+            print('Don\'t be a jerk this time.')
+            sleep(5)
+            users()
+        
+    except ValueError:
+        cls()
+        print('Please enter only integers')
+        sleep(3)
+        users()
+        
+    
+    
+
+
+
+
+
+
+
+
+
+def welcome():
+    print('Welcome to the Merryweather ATM setup wizard.\n\nPlease enter the following questions carefully for any wrong information may create further problems.')
+
+    sleep(5)
     print('')
-    t_name = o_name.title()
-    names.append(t_name)
-    with open('asset/name{}.py'.format(str(inx)), 'w') as nn:
-        nn.write('name{} = \'{}\''.format(inx, t_name))
-        nn.close()
-        inx += 1
-        usr += 1
-print('')
-print('Saving Names in Database')
-sleep(3)
-cls()
 
+    print('Gathering information', end = '')
+    
+    sleep(0.5)
+    print('.', end = '')
+    sleep(0.5)
+    print('.', end = '')
+    sleep(0.5)
+    print('.')
 
-inx2 = 0
+    sleep(2)
+    print('')
 
+    print('Compiling data', end = '')
+    
+    sleep(0.5)
+    print('.', end = '')
+    sleep(0.5)
+    print('.', end = '')
+    sleep(0.5)
+    print('.')
 
+    print('')
+    sleep(2)
+    print('Processing', end = '')
+    
+    sleep(0.5)
+    print('.', end = '')
+    sleep(0.5)
+    print('.', end = '')
+    sleep(0.5)
+    print('.')
+    
+    sleep(2)
+    cls()
+    users()
 
-for i in range(5):
-    try:
-        o_pin = int(input('Enter the preferred PIN for {}.\n> '.format(names[inx2])))
+names = []
+
+def name():
+    inx = 0
+    usr = 1
+    
+    for i in range(f):
+        o_name = input('Enter the name of user {}.\n> '.format(usr))
         print('')
-        with open('asset/pin{}.py'.format(str(inx2)), 'w') as pn:
-            pn.write('pin{} = {}'.format(inx2, o_pin))
-            pn.close()
-            inx2 += 1
+        t_name = o_name.title()
+        names.append(t_name)
+        with open('asset/name{}.py'.format(str(inx)), 'w') as nn:
+            nn.write('name{} = \'{}\''.format(inx, t_name))
+            nn.close()
+            inx += 1
+            usr += 1
+            cls()
+    print('Saving Names in Database')
+    sleep(3)
+    cls()
+    pin()
+
+
+
+def pin():
+    inx2 = 0
+    for i in range(f):
+        try:
+            o_pin = int(input('Enter the preferred PIN for {}.\n> '.format(names[inx2])))
+            print('')
+            o_pin1 = str(o_pin).startswith('0')
+            if o_pin1 == True:
+                cls()
+                print("Please avoid using \'0\' as the first digit.")
+                sleep(3)
+                cls()
+                pin()
+            else:
+                cls()
+                with open('asset/pin{}.py'.format(str(inx2)), 'w') as pn:
+                    pn.write('pin{} = {}'.format(inx2, o_pin))
+                    pn.close()
+                    inx2 += 1
+        except ValueError:
+            cls()
+            print('Please enter only numbers.')
+            sleep(3)
+            pin()
+    print('Saving PINs in Database')
+    sleep(3)
+    cls()
+    bal()
+         
+
+
+
+
+
+
+def bal():
+    inx3 = 0
+    for i in range(f):
+        try:
+            o_bal = int(input('Enter the Balance for {}.\n> $ '.format(names[inx3])))
+            print('')
+            with open('asset/asset{}.py'.format(str(inx3)), 'w') as bl:
+                bl.write('a{} = {}'.format(inx3, o_bal))
+                bl.close()
+                inx3 += 1
+                cls()
             
-    except ValueError:
-        cls()
-        print('You corrupted the data files by not being careful while entering data. Run this script again but if it doesn\'t run, reinstall the whole script')
-        sleep(10)
-        exit()
-
-print('')
-print('Saving PINs in Database')
-sleep(3)
-cls()
-
-
-inx3 = 0
-
-
-for i in range(5):
-    try:
-        o_bal = int(input('Enter the Balance for {}.\n> $ '.format(names[inx3])))
-        print('')
-        with open('asset/asset{}.py'.format(str(inx3)), 'w') as bl:
-            bl.write('a{} = {}'.format(inx3, o_bal))
-            bl.close()
-            inx3 += 1
-            
-    except ValueError:
-        cls()
-        print('You corrupted the data files by not being careful while entering data. Run this script again but if it doesn\'t run, reinstall the whole script')
-        sleep(10)
-        exit()
+        except ValueError:
+            cls()
+            print('Please enter only numbers.')
+            sleep(3)
+            bal()
+    done()
 
 
 
-cls()
+def done():
+    print('Configuration successful!')
+    print('')
+    print('You can now run the ATM.py script and login with the credentials you entered.')
+    sleep(8)
+    run()
 
-print('Configuration successful')
-print('')
-print('You can now run the ATM.py script and login with the credentials you entered.')
-sleep(8)
 def run():
     cls()
     atm = input('Would you like to run the ATM script now?\n> ')
@@ -155,13 +382,4 @@ def run():
         sleep(3)
         run()
     
-run()
-
-
-
-
-
-
-
-
-
+welcome()
